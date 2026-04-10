223b277d5bfdf7ec15202906ed2ecd78f9195954
emmanuel mukumbwa <emukumbwa2419@gmail.com>
2026-04-10 17:59:13 +0200

    feat(contact): add standalone contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import ContactForm from "@/components/ContactForm";
+
+export default function ContactPage() {
+  return (
+    <Container className="py-5">
+      <h1 className="mb-4 text-center">Contact Us</h1>
+      <div className="mx-auto" style={{ maxWidth: 600 }}>
+        <ContactForm inline buttonText="Send Message" />
+      </div>
+    </Container>
+  );
+}
